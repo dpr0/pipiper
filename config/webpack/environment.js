e23dcd6ac33e989a571812ed9b6ffeb3dd1976f7
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-12-28 11:53:54 +0300

    pipiper

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,5 +8,14 @@ environment.plugins.append('Provide',
         Popper: ['popper.js', 'default']
     })
 )
-module.exports = environment
 
+const globImporter = require('node-sass-glob-importer');
+
+environment
+    .loaders
+    .get('sass')
+    .use
+    .find(item => item.loader === 'sass-loader')
+    .options = { sassOptions: { importer: globImporter() } }; // <-- this!
+
+module.exports = environment
