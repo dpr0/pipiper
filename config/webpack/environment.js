e1ee848237d94190903a8cc4d4457d5e4b8351b6
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-12-28 12:42:22 +0300

    pipiper

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,13 +2,13 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 
-environment.plugins.append('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery/src/jquery',
-        Popper: ['popper.js', 'default']
-    })
-)
+// environment.plugins.append('Provide',
+//     new webpack.ProvidePlugin({
+//         $: 'jquery',
+//         jQuery: 'jquery/src/jquery',
+//         Popper: ['popper.js', 'default']
+//     })
+// )
 
 const globImporter = require('node-sass-glob-importer');
 
