5b5ac68bcd5035b3e719428c6796b878d4df77f3
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-12-31 09:27:55 +0300

    pipiper-without_firebase

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,12 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Piedpiper
+module Pipiper
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :ru
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
