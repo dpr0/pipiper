853c8b25898bef9d466a71db296e9e761b5a7333
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2022-01-16 21:18:25 +0300

    invite

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'gmail.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 465,
-    user_name:            ENV['GMAIL_USER'],
+    port:                 587,
+    user_name:            ENV['GMAIL_USER'] + '@gmail.com',
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       :plain,
     enable_starttls_auto: true
