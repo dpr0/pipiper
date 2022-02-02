1e47a5a56653be411965059192ef272474cf068d
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2022-02-02 08:14:27 +0300

    rubocop

--- config/application.rb
+++ config/application.rb
@@ -8,24 +8,19 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Configuration for the application, engines, and railties goes here.
-#
-# These settings can be overridden in specific environments using the files
-# in config/environments, which are processed later.
-#
-# config.time_zone = "Central Time (US & Canada)"
-# config.eager_load_paths << Rails.root.join("extras")
-class Famitree::Application < Rails::Application
-  # Initialize configuration defaults for originally generated Rails version.
-  config.load_defaults 7.0
-  config.i18n.default_locale = :ru
-  config.read_encrypted_secrets = (Rails.env == 'production')
+module Famitree
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+    config.i18n.default_locale = :ru
+    config.read_encrypted_secrets = (Rails.env == 'production')
 
-  # Configuration for the application, engines, and railties goes here.
-  #
-  # These settings can be overridden in specific environments using the files
-  # in config/environments, which are processed later.
-  #
-  # config.time_zone = "Central Time (US & Canada)"
-  # config.eager_load_paths << Rails.root.join("extras")
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
 end
