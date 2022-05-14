1d97a295094bd3c69281166aa252ccdb0b567e39
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2022-05-14 12:28:46 +0300

    cap

--- app/services/action_service.rb
+++ app/services/action_service.rb
@@ -39,7 +39,8 @@ class ActionService
           mq('defafon/v1/in', cap[:state][:instance] == 'on' ? 'O' : 'N')
         end
       else
-        resp = RestClient.post("http://#{ud.host}:#{ud.port}/#{dc.path}", { pin: dc.pin, status: cap[:state][:value] }.to_json)
+        resp = RestClient.get("http://#{ud.host}:#{ud.port}/#{dc.path}/toggle_pin/#{dc.pin}")
+        # resp = RestClient.post("http://#{ud.host}:#{ud.port}/#{dc.path}", { pin: dc.pin, status: cap[:state][:value] }.to_json)
         resp.code
       end
       # response = JSON.parse(resp.body)
