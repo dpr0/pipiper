815fce2ba9d0678c491ef30626989cf219676666
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2022-09-19 10:55:09 +0300

    araz

--- app/services/debtors_service.rb
+++ app/services/debtors_service.rb
@@ -3,27 +3,25 @@ require 'csv'
 class DebtorsService
   def initialize
     @data = []
-    @codes = [(1..78).to_a, 86, 89, 82, 92].flatten.map { |x| x.to_s.rjust(2, '0') }
+    @codes = [74] # [(1..78).to_a, 86, 89, 82, 92].flatten.map { |x| x.to_s.rjust(2, '0') }
   end
 
   def call
-    # (1..3).each { |x| process(x) }
-    process
+    (1..7).each { |x| process(x) }
 
-    csv_string = CSV.generate do |csv|
+    CSV.generate do |csv|
       csv << ['ФИО', 'Дата рождения', 'Код региона']
       @data.each do |data|
         csv << data
       end
     end
-    csv_string
   end
 
   private
 
-  def process
+  def process(num)
     @codes.each do |code|
-      resp = send_request(code)
+      resp = send_request(code, num)
       next unless resp
 
       json = JSON.parse resp
@@ -33,7 +31,12 @@ class DebtorsService
         title = tag.css('.b-personnel__card-title')[0].text.split(',')[0]
         text = tag.css('.b-personnel__card-text')[0].text
         date = DatesFromString.new.find_date(text)[0] || DatesFromString.new.find_date(title)[0]
-        @data << [title, date.nil? ? '' : date.to_date.strftime('%d.%m.%Y'), code]
+        begin
+          dt = date&.to_date ? date.to_date.strftime('%d.%m.%Y') : ''
+        rescue
+          ''
+        end
+        @data << [title, dt, code]
       end
     end
     # page = parsed_data.xpath('//a').last.attributes['href'].text.split('page=').last.split('&').first.to_i
@@ -42,7 +45,7 @@ class DebtorsService
   def send_request(code, page = nil)
     link = "https://fssp.gov.ru/vnimanie_rozysk/action/debtors/?#{"page=#{page}&" if page}region=#{code}"
     print code
-    z = RestClient.get(link)
+    z = RestClient.get(link, 'X-Requested-With': 'XMLHttpRequest')
     puts
     z
   rescue
